b50963bcc5217b23d9881de47ae62f34cf217b22
Ester <esterbiziu@gmail.com>
2025-06-07 22:18:58 -0300

    implementa cadastro em duas etapas com DTO, validações e navegação

---
+++ app/src/main/java/com/tcc/money/data/dto/Cadastro.kt
@@ -0,0 +1,12 @@
+package com.tcc.money.data.dto
+
+import java.io.Serializable
+
+data class Cadastro(
+    val nome: String,
+    val sobrenome: String,
+    val cpf: String,
+    val dataNascimento: String,
+    val email: String = "",
+    val senha: String = ""
+) : Serializable
